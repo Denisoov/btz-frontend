092f5d4247647923d397008a6b9268ff616cb33f
ikita enisov <denisoov07@gmail.com>
2022-04-07 13:10:26 +0500

    Инициализация проекта

---
+++ nuxt.config.js
@@ -0,0 +1,37 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  head: {
+    titleTemplate: '%s - btz-frontend',
+    title: 'btz-frontend',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
+
+  modules: [],
+
+  vuetify: {
+    treeShake: true,
+    defaultAssets: false,
+  },
+
+  build: {},
+}
